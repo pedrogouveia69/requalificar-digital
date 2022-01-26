0b98188eaf2021af53a14ae2b0984d3f2aff8efd
Pedro Gouveia <58569642+pedrogouveia69@users.noreply.github.com>
2022-01-26 14:07:00 +0000

    fx

--- Ficha6/Ficha6/People.cs
+++ Ficha6/Ficha6/People.cs
@@ -3,10 +3,6 @@
     public class People
     {
         public List<Person> PersonList { get; set;}
-        public void Add(Person person)
-        {
-            PersonList.Add(person);
-        }
 
     }
 
