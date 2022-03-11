3a683c2e9398d1466fd5a78841cff1397611cd60
Pedro Gouveia <58569642+pedrogouveia69@users.noreply.github.com>
2022-03-11 17:56:34 +0000

    mvc

---
+++ dotnet/MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Ficha10;
+using Ficha10.Controllers;
+using Ficha7Saturday;
+using FREQ_2019419;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System.Diagnostics;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var cc = new EmployeesController();
+            return View(new Employees(cc.Get().ToList()));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ dotnet/MVC/MVC/Models/Employees.cs
@@ -0,0 +1,23 @@
+﻿using Ficha10;
+using Ficha10.Models;
+
+namespace Ficha7Saturday
+{
+    public class Employees : IEmployees
+    {
+        private List<Employee> employeesList;
+
+        public Employees()
+        {
+            employeesList = JsonLoader.DeserializeEmployees();
+        }
+
+        public Employees(List<Employee> employeesList)
+        {
+            this.employeesList = employeesList;
+        }
+
+        public List<Employee> EmployeesList { get { return employeesList; } set { employeesList = value; } }
+        
+    }
+}
