34c035414089849171d200e3dc5a68ee5d2aad59
Pedro Gouveia <58569642+pedrogouveia69@users.noreply.github.com>
2022-03-13 11:27:44 +0000

    tpc

--- dotnet/MVC/MVC/Controllers/JsonLoader.cs
+++ dotnet/MVC/MVC/Controllers/JsonLoader.cs
@@ -1,6 +1,5 @@
 ﻿using Ficha10.Models;
 using Ficha7Saturday;
-using FREQ_2019419;
 using System.Text.Json;
 
 namespace Ficha10
@@ -9,12 +8,15 @@ namespace Ficha10
     {
         public static List<Employee> DeserializeEmployees()
         {
-            return JsonSerializer.Deserialize<List<Employee>>(File.ReadAllText("employees.json"));
-        }
-
-        public static Characters DeserializeCharacters()
-        {
-            return JsonSerializer.Deserialize<Characters>(File.ReadAllText("characters.json"));
+            try
+            {
+                return JsonSerializer.Deserialize<List<Employee>>(File.ReadAllText("employees.json"));
+            }
+            catch(FileNotFoundException e)
+            {
+                File.WriteAllText("employees.json", "[]");
+                return new List<Employee>();
+            }
         }
     }
 }
