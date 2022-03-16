b89d9edeff955993f912f902075635e286edb6b2
Pedro Gouveia <58569642+pedrogouveia69@users.noreply.github.com>
2022-03-16 17:51:40 +0000

    firebase

--- front_end/project-nismo/src/main.js
+++ front_end/project-nismo/src/main.js
@@ -1,24 +1,38 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// import Vue from 'vue'
+// import BootstrapVue from "bootstrap-vue"
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap-vue/dist/bootstrap-vue.css"
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB_djwnz13dZi4GndGj3BPQ85aQGdkF9U0",
-//   authDomain: "proj-web1-8efd2.firebaseapp.com",
-//   projectId: "proj-web1-8efd2",
-//   storageBucket: "proj-web1-8efd2.appspot.com",
-//   messagingSenderId: "345030686286",
-//   appId: "1:345030686286:web:e7fceb1a748f24957eb51f"
-// };
+// Vue.use(BootstrapVue)
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_djwnz13dZi4GndGj3BPQ85aQGdkF9U0",
+  authDomain: "proj-web1-8efd2.firebaseapp.com",
+  projectId: "proj-web1-8efd2",
+  storageBucket: "proj-web1-8efd2.appspot.com",
+  messagingSenderId: "345030686286",
+  appId: "1:345030686286:web:e7fceb1a748f24957eb51f"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
 
--- front_end/project-nismo/src/views/LoginView.vue
+++ front_end/project-nismo/src/views/LoginView.vue
@@ -1,42 +1,51 @@
 <template>
-  <form style="margin-top: 300px">
-    <input v-model="email" type="email" placeholder="Email" required="true" />
-    <input
-      v-model="password"
-      type="password"
-      placeholder="Password"
-      required="true"
-      minlength="6"
-    />
-    <button @click="signupRequest()">Register</button>
-  </form>
+  <h1 style="margin-top: 300px">Create an Account</h1>
+  <p><input type="text" placeholder="Email" v-model="email" /></p>
+  <p><input type="password" placeholder="Password" v-model="password" /></p>
+  <p><button @click="register()">Submit</button></p>
+  <br />
+  <br />
+  <h1>Login</h1>
+  <p><input type="text" placeholder="Email" v-model="email_login" /></p>
+  <p><input type="password" placeholder="Password" v-model="password_login" /></p>
+  <p><button @click="login()">Submit</button></p>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      email: "",
-      password: "",
-    };
-  },
-  methods: {
-    signupRequest() {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(this.email, this.password)
-        .then(
-          () => {
-            this.successMessage = "Register Successfully.";
-            alert(this.successMessage);
-          },
-          (error) => {
-            let errorResponse = JSON.parse(error.message);
-            this.errorMessage = errorResponse.error.message;
-            alert(this.errorMessage);
-          }
-        );
-    },
-  },
+<script setup>
+import { ref } from "vue";
+import firebase from "firebase";
+import { useRouter } from "vue-router"; // import router
+const email = ref("");
+const password = ref("");
+const email_login = ref("");
+const password_login = ref("");
+const router = useRouter(); // get a reference to our vue router
+
+const register = () => {
+  firebase
+    .auth() // get the auth api
+    .createUserWithEmailAndPassword(email.value, password.value) // need .value because ref()
+    .then((data) => {
+      console.log("Successfully registered!");
+      router.push("/"); // redirect to the feed
+    })
+    .catch((error) => {
+      console.log(error.code);
+      alert(error.message);
+    });
+};
+
+const login = () => {
+  firebase
+    .auth() // get the auth api
+    .signInWithEmailAndPassword(email_login.value, password_login.value) // need .value because ref()
+    .then((data) => {
+      console.log("Successfully logged in!");
+      router.push("/"); // redirect to the feed
+    })
+    .catch((error) => {
+      console.log(error.code);
+      alert(error.message);
+    });
 };
 </script>
